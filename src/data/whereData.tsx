fe2ed874e2cc276ff4a83753774d660bd464acc6
Danish <s.danish0827@gmail.com>
2024-10-22 14:09:03 +0530

    Sad

--- src/data/whereData.tsx
+++ src/data/whereData.tsx
@@ -1,9 +1,9 @@
 import where1 from "@/assets/images/where/1.jpeg";
 import where2 from "@/assets/images/where/2.jpeg";
 import where3 from "@/assets/images/where/3.jpg";
-// import where4 from "@/assets/images/where/4.jpg";
-// import where5 from "@/assets/images/where/5.jpg";
-// import where6 from "@/assets/images/where/6.jpg";
+import where4 from "@/assets/images/where/4.jpeg";
+import where5 from "@/assets/images/where/5.jpg";
+import where6 from "@/assets/images/where/6.jpg";
 // import where7 from "@/assets/images/where/7.jpg";
 // import where8 from "@/assets/images/where/8.jpg";
 // import where9 from "@/assets/images/where/9.jpg";
@@ -38,9 +38,9 @@ const whereDatas = [
   { id: 1, image: where1, heading: "School" },
   { id: 2, image: where2, heading: "School" },
   { id: 3, image: where3, heading: "PROJECT 1" },
-//   { id: 4, image: where4, heading: "PROJECT 2" },
-//   { id: 5, image: where5, heading: "PROJECT 3" },
-//   { id: 6, image: where6, heading: "PROJECT 4" },
+  { id: 4, image: where4, heading: "PROJECT 2" },
+  { id: 5, image: where5, heading: "PROJECT 3" },
+  { id: 6, image: where6, heading: "PROJECT 4" },
 //   { id: 7, image: where7, heading: "PROJECT 5" },
 //   { id: 8, image: where8, heading: "PROJECT 6" },
 //   { id: 9, image: where9, heading: "PROJECT 7" },

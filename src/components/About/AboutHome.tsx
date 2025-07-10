6f6119375f7d0c54a778995d1f3a9c37de3db6ad
Danish Shaikh <94749064+Danish0827@users.noreply.github.com>
2025-07-10 17:48:19 +0530

    Update AboutHome.tsx

--- src/components/About/AboutHome.tsx
+++ src/components/About/AboutHome.tsx
@@ -83,7 +83,7 @@ const AboutHome = () => {
             </span> */}
             <p className="text-lg text-[#000] my-4 text-justify font-normal">
               I am Arpit Bansal, a doctor, specialized in advanced laparoscopic,
-              
+              {" "}
               <Link
                 className="text-[#232c77] font-semibold"
                 href="https://drarpitbansal.in/speciality/best-cancer-treatment-in-india"
